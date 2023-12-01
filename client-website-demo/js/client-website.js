08ae8911cb1cf84a4e1accc248ea75d4028015bf
Dontae Brown <143196337+2023899@users.noreply.github.com>
2023-12-01 09:08:47 -0500

    Delete client-website-demo directory

--- client-website-demo/js/client-website.js
+++
@@ -1,8 +0,0 @@
-// Theme switcher selection
-const checkbox = document.querySelector("#checkbox");
-
-// Theme switcher functionality
-checkbox.addEventListener("change", () => {
-    // Toggle website theme
-    document.body.classList.toggle("dark");
-  });
